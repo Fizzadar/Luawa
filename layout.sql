9c0ec95277f01b7490ab66928649ecdf75c61fcc
Nick Barrett <nick@oxygem.com>
2014-05-07 20:33:08 +0100

    Remove unused columns

--- layout.sql
+++ layout.sql
@@ -13,65 +13,42 @@
 
 # Dump of table user
 # ------------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(128) NOT NULL DEFAULT '' COMMENT 'used to identify',
-  `password` varchar(255) NOT NULL DEFAULT '' COMMENT 'hashed pw',
-  `salt` varchar(255) NOT NULL DEFAULT '' COMMENT 'salt for hash',
-  `group` tinyint(3) unsigned NOT NULL DEFAULT '2' COMMENT '1=admin;2=user;2>=custom',
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'name/display only',
-  `login_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `register_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `password_reset_key` varchar(255) NOT NULL DEFAULT '' COMMENT 'pwreset temp key',
-  `password_reset_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'expire time on pwreset',
-  `key1` varchar(255) NOT NULL DEFAULT '',
-  `key2` varchar(255) NOT NULL DEFAULT '',
-  `key3` varchar(255) NOT NULL DEFAULT '',
-  `real_name` varchar(255) NOT NULL DEFAULT '',
-  `address` text NOT NULL,
-  `country` varchar(3) NOT NULL DEFAULT '',
-  `phone` int(10) unsigned NOT NULL DEFAULT '0',
-  `credit` int(10) unsigned NOT NULL DEFAULT '0',
-  `two_factor` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `pubkey` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(128) NOT NULL DEFAULT '' COMMENT 'used to identify',
+    `password` varchar(255) NOT NULL DEFAULT '' COMMENT 'hashed pw',
+    `salt` varchar(255) NOT NULL DEFAULT '' COMMENT 'salt for hash',
+    `group` tinyint(3) unsigned NOT NULL DEFAULT '2' COMMENT '1=admin;2=user;2>=custom',
+    `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'name/display only',
+    `login_time` int(10) unsigned NOT NULL DEFAULT '0',
+    `register_time` int(10) unsigned NOT NULL DEFAULT '0',
+    `password_reset_key` varchar(255) NOT NULL DEFAULT '' COMMENT 'pwreset temp key',
+    `password_reset_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'expire time on pwreset',
+    `key1` varchar(255) NOT NULL DEFAULT '',
+    `key2` varchar(255) NOT NULL DEFAULT '',
+    `key3` varchar(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table user_groups
 # ------------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `user_groups` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+    `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-# Dump of table user_messages
-# ------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `user_messages` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table user_permissions
 # ------------------------------------------------------------
-
 CREATE TABLE IF NOT EXISTS `user_permissions` (
-  `group` tinyint(3) unsigned NOT NULL,
-  `permission` varchar(64) NOT NULL DEFAULT '',
-  UNIQUE KEY `group_id` (`group`,`permission`),
-  CONSTRAINT `permissions-user_groups_id` FOREIGN KEY (`group`) REFERENCES `user_groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    `group` tinyint(3) unsigned NOT NULL,
+    `permission` varchar(64) NOT NULL DEFAULT '',
+    UNIQUE KEY `group_id` (`group`,`permission`),
+    CONSTRAINT `permissions-user_groups_id` FOREIGN KEY (`group`) REFERENCES `user_groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
